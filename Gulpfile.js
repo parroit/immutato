6e4fd6f812719201bc7b2229506bba4c0842cb8e
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-11-12 18:57:09 +0100

    add istanbul coverage

--- Gulpfile.js
+++ Gulpfile.js
@@ -14,10 +14,25 @@ var $ = loadPlugins({
     lazy: true
 });
 
+var tests = 'test/*.js';
+var sources = 'lib/*.js';
+
 gulp.task('test', function () {
-  return gulp.src('./test/*.js')
+  return gulp.src(tests)
     .pipe($.mocha({grep:'@perf', invert: true}));
 });
+
+gulp.task('cover', function (cb) {
+  gulp.src(sources)
+    .pipe($.istanbul()) // Covering files
+    .on('finish', function () {
+      gulp.src(tests)
+        .pipe($.mocha())
+        .pipe($.istanbul.writeReports()) // Creating the reports after tests runned
+        .on('end', cb);
+    });
+});
+
 
 gulp.task('perf', function () {
   return gulp.src('./test/*.js')
