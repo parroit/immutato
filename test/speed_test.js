a1402cab699842ecd8e87cfe4b8d205d19b9dc69
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-11-19 17:16:15 +0100

    removed bug of 100 undo version

--- test/speed_test.js
+++ test/speed_test.js
@@ -13,7 +13,7 @@ chai.expect();
 chai.should();
 
 var immutato = require('../lib/immutato.js');
-
+var I = require('../devel/lib/immutato.js');
 
 describe('immutato', function() {
     var Imm;
@@ -64,6 +64,18 @@ describe('immutato', function() {
             }
         });
 
+        profile('create immutato NEW VERSION', function() {
+            for (i = 0; i < 10000; i++) {
+                imm = I({
+                    name: 'Andrea',
+                    age: i
+                });
+
+                imm.name().should.be.equal('Andrea');
+                imm.age().should.be.equal(i);
+            }
+        });
+
 
         profile('create immutato object & change property', function() {
             imm = new Imm({
@@ -90,6 +102,18 @@ describe('immutato', function() {
             }
         });
 
+        profile('create immutato NEW VERSION & change property', function() {
+            for (i = 0; i < 1000; i++) {
+                imm = I({
+                    name: 'Andrea',
+                    age: i
+                });
+
+                imm = imm.age(42);
+                imm.age().should.be.equal(42);
+            }
+        });
+
     });
 
     it('repeated set property @perf', function() {
@@ -100,7 +124,7 @@ describe('immutato', function() {
         var fields = {
             ciao: immutato.String
         };
-        var iterations = 1000;
+        var iterations = 10000;
         var i = 0;
 
         for (; i < 100; i++) {
@@ -111,15 +135,13 @@ describe('immutato', function() {
         var Struct = immutato.struct(fields);
 
         var o = new Struct(data);
+        var inv = I(data);
         var test;
 
 
         profile('Set property using prototype chain', function() {
 
             var i = 0;
-
-
-
             for (; i < iterations; i++) {
                 o = o.set('ciao', 'ciao' + i);
             }
@@ -127,14 +149,30 @@ describe('immutato', function() {
 
 
         profile('Read property using prototype chain', function() {
+
             var i = 0;
-            for (; i < iterations; i++) {
+            for (; i < iterations/10; i++) {
                 test = o.ciao;
             }
         });
 
 
+        profile('Set property using transactions', function() {
 
+            var i = 0;
+            for (; i < iterations; i++) {
+                inv = inv.ciao('ciao' + i);
+            }
+        });
+
+
+        profile('Read property using transactions', function() {
+
+            var i = 0;
+            for (; i < iterations/10; i++) {
+                test = inv.ciao();
+            }
+        });
     });
 
 });
